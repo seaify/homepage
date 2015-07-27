8d9a7fcb7168b0eae3e1116eb0bfc9377e1a024b
seaify <dilin.life@gmail.com>
2015-07-27 17:02:49 +0800

    update

--- spec/factories/topics.rb
+++ spec/factories/topics.rb
@@ -3,8 +3,8 @@
 # Table name: topics
 #
 #  id         :integer          not null, primary key
-#  title      :string
-#  body       :text
+#  title      :string(255)
+#  body       :text(65535)
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
