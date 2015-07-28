1abd26ef739fea9682e231c59c1d41b6f9ba63e7
seaify <dilin.life@gmail.com>
2015-07-28 10:09:18 +0800

    update

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,7 +1,7 @@
 app_path = File.expand_path( File.join(File.dirname(__FILE__), '..'))
 worker_processes   2
 timeout            180
-listen             3001
+listen             '/tmp/homepage.unicorn.sock'
 pid                "#{app_path}/tmp/pids/unicorn.pid"
 stderr_path        "log/unicorn.log"
 stdout_path        "log/unicorn.log"
